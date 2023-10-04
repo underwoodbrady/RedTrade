86ce57264547d0c19f86d65b484c6a51fd368c69
Brady Underwood <underwood.brady@gmail.com>
2023-10-04 01:43:41 -0600

    FAQ works

--- src/lib/components/util/FAQCard.svelte
+++ src/lib/components/util/FAQCard.svelte
@@ -1,9 +1,126 @@
 <script lang="ts">
-    export let question:string;
-    export let answer:string;
+	export let question: string;
+	export let answer: string;
+	export let opened: boolean = false;
+	export let openedOnce: boolean = false; //Probably dumb solution but whatever
 </script>
 
-<div class="bg-darker/90 w-full h-24 rounded-md drop-shadow-md flex items-center px-10 justify-between cursor-pointer">
-    <h4 class="text-neutral-200 text-2xl font-semibold">{question}</h4>
-    <img src="/plus-white.svg" alt="Plus Icon" class="w-9"/>
+<div
+	class={!openedOnce
+		? 'text-neutral-200 bg-darker/90 w-full rounded-md drop-shadow-md flex flex-col px-10 py-8 cursor-pointer'
+		: opened
+		? 'open-anim text-neutral-200 bg-darker/90 w-full rounded-md drop-shadow-md flex flex-col px-10 py-8 cursor-pointer'
+		: 'close-anim text-neutral-200 bg-darker/90 w-full rounded-md drop-shadow-md flex flex-col px-10 py-8 cursor-pointer'}
+>
+	<button
+		on:click={() => {
+			opened = !opened;
+			openedOnce = true;
+		}}
+		class="relative flex justify-left w-full"
+	>
+		<h4 class=" text-2xl font-semibold">{question}</h4>
+		{#if opened && openedOnce}
+			<img
+				src="/minus-white.svg"
+				alt="
+        Minus Icon"
+				class="absolute w-6 right-2 top-4"
+			/>
+		{:else}
+			<img src="/plus-white.svg" alt="Plus Icon" class="absolute w-9 right-0 top-0" />
+		{/if}
+	</button>
+
+	<div
+		class={!openedOnce
+			? 'h-0 overflow-hidden'
+			: opened
+			? 'stretch-anim overflow-hidden'
+			: 'thin-anim h-0 overflow-hidden'}
+	>
+		<p class="text-white text-xl">{answer}</p>
+	</div>
 </div>
+
+<style>
+	.open-anim {
+		animation-name: open;
+		animation-duration: 0.05s;
+		animation-direction: normal;
+		animation-timing-function: linear;
+		animation-iteration-count: 1;
+		animation-fill-mode: forwards;
+	}
+
+	.stretch-anim {
+		animation-name: stretch;
+		animation-duration: 0.05s;
+		animation-direction: normal;
+		animation-timing-function: linear;
+		animation-iteration-count: 1;
+		animation-fill-mode: forwards;
+	}
+
+	.close-anim {
+		animation-name: close;
+		animation-duration: 0.05s;
+		animation-direction: normal;
+		animation-timing-function: linear;
+		animation-iteration-count: 1;
+		animation-fill-mode: forwards;
+	}
+
+	.thin-anim {
+		animation-name: thin;
+		animation-duration: 0.05s;
+		animation-direction: normal;
+		animation-timing-function: linear;
+		animation-iteration-count: 1;
+		animation-fill-mode: forwards;
+	}
+
+	@keyframes open {
+		0% {
+			color: white;
+			background-color: rgb(107 60 60 / 0.9);
+		}
+		100% {
+			color: #eb3d3d;
+			background-color: rgba(122, 66, 66, 0.9);
+		}
+	}
+
+	@keyframes close {
+		0% {
+			color: #eb3d3d;
+			background-color: rgba(122, 66, 66, 0.9);
+		}
+		100% {
+			color: white;
+			background-color: rgb(107 60 60 / 0.9);
+		}
+	}
+
+	@keyframes stretch {
+		0% {
+			margin-top: 0px;
+			height: 0px;
+		}
+		100% {
+			margin-top: 24px;
+			height: auto;
+		}
+	}
+
+	@keyframes thin {
+		0% {
+			margin-top: 24px;
+			height: auto;
+		}
+		100% {
+			margin-top: 0px;
+			height: 0px;
+		}
+	}
+</style>
--- src/routes/FAQ/+page.svelte
+++ src/routes/FAQ/+page.svelte
@@ -7,12 +7,12 @@
 	<div class="absolute left-0 right-0 top-0 bottom-0">
 		<div class="backgroundgrad w-full h-full" />
 	</div>
-	<h3 class="text-white font-semibold text-5xl mb-20 z-20">Frequently Asked Questions</h3>
+	<h3 class="text-white font-bold text-5xl mb-20 z-20">Frequently Asked Questions</h3>
     <section class="flex flex-col space-y-8 w-full max-w-6xl">
-        <FAQCard question="What is Red Trade?" answer=""/>
-        <FAQCard question="How is my money managed?" answer=""/>
-        <FAQCard question="Why is it better than other platforms?" answer=""/>
-        <FAQCard question="What's on the roadmap?" answer=""/>
+        <FAQCard question="What is Red Trade?" answer="Red Trade is a portfolio visualization company specifically for retail investors"/>
+        <FAQCard question="How is my money managed?" answer="Your money is managed through your prefered broker, we just find new ways to display it to you."/>
+        <FAQCard question="Why is it better than other platforms?" answer="Figure it out for yourself"/>
+        <FAQCard question="What's on the roadmap?" answer="Every day a new feature is added. Click here to see what's been added recently"/>
     </section>
 </main>
 
