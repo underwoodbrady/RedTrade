3a2a3b7e9693350cfff1acb692476850177584bb
Brady Underwood <underwood.brady@gmail.com>
2023-10-06 15:29:53 -0600

    setup for future widgets

--- src/routes/(authed)/app/+page.svelte
+++ src/routes/(authed)/app/+page.svelte
@@ -1,4 +1,4 @@
-<main class="flex flex-1 bg-neutral-800 text-white text-lg px-12 py-4">
+<main class="flex flex-1 flex-wrap bg-neutral-800 text-white text-lg px-12 py-4">
 	<div
 		class="relative bg-[#303030] rounded-md drop-shadow-md p-6 w-80 h-52 flex flex-col justify-between"
 	>
@@ -33,14 +33,14 @@
 		</div>
 		<div class="flex space-x-6">
 			<div class="flex flex-col">
-                <div class="flex items-center space-x-2">
+				<div class="flex items-center space-x-2">
 					<img src="/arrow-green.svg" alt="Arrow Green Up" class="h-[14px]" />
 					<h5 class="text-xl text-green-500 font-semibold">1.2%</h5>
 				</div>
 				<p class="text-neutral-400 font-normal text-sm">Last 24hr</p>
 			</div>
 			<div class="flex flex-col">
-                <div class="flex items-center space-x-2">
+				<div class="flex items-center space-x-2">
 					<img src="/arrow-red.svg" alt="Arrow Red Down" class="h-[14px]" />
 
 					<h5 class="text-xl text-red-500 font-semibold">0.6%</h5>
@@ -64,15 +64,17 @@
 		</div>
 		<div class="flex items-center space-x-6">
 			<h4 class="font-semibold text-neutral-200">Single Stock</h4>
-			<div class="relative rounded-full bg-neutral-700 w-24 py-1 px-4 flex justify-between items-center font-semibold">
-				<p class="text-sm text-neutral-200 ">GME</p>
-                <img src="/chevron.svg" alt="Down Icon" class="h-4"/>
+			<div
+				class="relative rounded-full bg-neutral-700 w-24 py-1 px-4 flex justify-between items-center font-semibold"
+			>
+				<p class="text-sm text-neutral-200">GME</p>
+				<img src="/chevron.svg" alt="Down Icon" class="h-4" />
 			</div>
 		</div>
 		<div class="flex items-center justify-between">
 			<h2 class="text-5xl font-bold tracking-wide">$23.12</h2>
-            <div class="flex flex-col">
-                <div class="flex items-center space-x-2">
+			<div class="flex flex-col">
+				<div class="flex items-center space-x-2">
 					<img src="/arrow-green.svg" alt="Arrow Green Up" class="h-3" />
 					<h5 class="text-lg text-green-500 font-semibold">1.2%</h5>
 				</div>
@@ -80,32 +82,92 @@
 			</div>
 		</div>
 		<div class="flex items-center justify-between">
-            <div class="flex flex-col">
-                <div class="flex items-center space-x-4">
-                    <p class="text-neutral-200 font-semibold text-sm">Open</p>
-                    <p class="text-neutral-400">$20.01</p>
-                </div>
-                <div class="flex items-center space-x-4">
-                    <p class="text-neutral-200 font-semibold text-sm">Close</p>
-                    <p class="text-neutral-400">$23.12</p>
-                </div>
-            </div>
-            <div class="flex flex-col">
-                <div class="flex items-center space-x-4">
-                    <p class="text-neutral-200 font-semibold text-sm">High</p>
-                    <p class="text-neutral-400">$24.99</p>
-                </div>
-                <div class="flex items-center space-x-4">
-                    <p class="text-neutral-200 font-semibold text-sm">Low</p>
-                    <p class="text-neutral-400">$19.60</p>
-                </div>
-            </div>
-            <div class="relative flex flex-col items-center ml-2">
-                <img src="/color-scale.svg" alt="Color Scale" class="h-10 rounded-full mb-1"/>
-                <p class="text-xs font-semibold text-neutral-400 -mb-4">Low</p>
-                <img src="/arrow-left.svg" alt="White Arrow Ticker" class="absolute left-[2px] top-2 h-2 rotate-180"/>
-
-            </div>
+			<div class="flex flex-col">
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">Open</p>
+					<p class="text-neutral-400">$20.01</p>
+				</div>
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">Close</p>
+					<p class="text-neutral-400">$23.12</p>
+				</div>
+			</div>
+			<div class="flex flex-col">
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">High</p>
+					<p class="text-neutral-400">$24.99</p>
+				</div>
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">Low</p>
+					<p class="text-neutral-400">$19.60</p>
+				</div>
+			</div>
+			<div class="relative flex flex-col items-center ml-2">
+				<img src="/color-scale.svg" alt="Color Scale" class="h-10 rounded-full mb-1" />
+				<p class="text-xs font-semibold text-neutral-400 -mb-4">Low</p>
+				<img
+					src="/arrow-left.svg"
+					alt="White Arrow Ticker"
+					class="absolute left-[2px] top-2 h-2 rotate-180"
+				/>
+			</div>
+		</div>
+	</div>
+	<div
+		class="relative bg-[#303030] rounded-md drop-shadow-md p-6 mt-8 w-80 h-52 flex flex-col justify-between"
+	>
+		<div class="absolute right-7 top-7">
+			<img src="/hamburger-menu.svg" alt="Grab and drop hamburger" class="w-5" />
+		</div>
+		<div class="flex items-center space-x-6">
+			<h4 class="font-semibold text-neutral-200">Single Stock</h4>
+			<div
+				class="relative rounded-full bg-neutral-700 w-24 py-1 px-4 flex justify-between items-center font-semibold"
+			>
+				<p class="text-sm text-neutral-200">GME</p>
+				<img src="/chevron.svg" alt="Down Icon" class="h-4" />
+			</div>
+		</div>
+		<div class="flex items-center justify-between">
+			<h2 class="text-5xl font-bold tracking-wide">$23.12</h2>
+			<div class="flex flex-col">
+				<div class="flex items-center space-x-2">
+					<img src="/arrow-green.svg" alt="Arrow Green Up" class="h-3" />
+					<h5 class="text-lg text-green-500 font-semibold">1.2%</h5>
+				</div>
+				<p class="text-neutral-400 font-normal text-sm">Last 24hr</p>
+			</div>
+		</div>
+		<div class="flex items-center justify-between">
+			<div class="flex flex-col">
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">Open</p>
+					<p class="text-neutral-400">$20.01</p>
+				</div>
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">Close</p>
+					<p class="text-neutral-400">$23.12</p>
+				</div>
+			</div>
+			<div class="flex flex-col">
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">High</p>
+					<p class="text-neutral-400">$24.99</p>
+				</div>
+				<div class="flex items-center space-x-4">
+					<p class="text-neutral-200 font-semibold text-sm">Low</p>
+					<p class="text-neutral-400">$19.60</p>
+				</div>
+			</div>
+			<div class="relative flex flex-col items-center ml-2">
+				<img src="/color-scale.svg" alt="Color Scale" class="h-10 rounded-full mb-1" />
+				<p class="text-xs font-semibold text-neutral-400 -mb-4">Low</p>
+				<img
+					src="/arrow-left.svg"
+					alt="White Arrow Ticker"
+					class="absolute left-[2px] top-2 h-2 rotate-180"
+				/>
+			</div>
 		</div>
 	</div>
 </main>
