11bc2e340676864b04f149fce9d535ce2e9140f3
Brady Underwood <underwood.brady@gmail.com>
2023-10-09 14:59:44 -0600

    dropdown no animation

--- src/lib/components/util/Dropdown.svelte
+++ src/lib/components/util/Dropdown.svelte
@@ -1,31 +1,43 @@
 <script lang="ts">
-    export let dropDownOpen:boolean = false;
+	export let dropDownOpen: boolean = false;
 </script>
 
 <button
-	class="relative rounded-full w-44 py-2 px-4 bg-[#303030] text-neutral-300 text-sm font-semibold flex space-x-4 items-center justify-between drop-shadow-sm hover:bg-neutral-700"
-    on:click={() => {
-        dropDownOpen = !dropDownOpen;
-    }}
+	class={dropDownOpen ? "relative rounded-tr-md rounded-tl-md rounded-br-none rounded-bl-none w-44 py-2 px-4 bg-neutral-700 flex space-x-4 items-center justify-between" :"relative rounded-full w-44 py-2 px-4 bg-[#303030] flex space-x-4 items-center justify-between drop-shadow-sm hover:bg-neutral-700"}
+	on:click={() => {
+		dropDownOpen = !dropDownOpen;
+	}}
 >
-	<p>Select</p>
-	<img src="/chevron.svg" alt="Down Icon" class="h-4" />
-    {#if dropDownOpen}
-        <div class="absolute top-full -left-4 bg-neutral-700 w-full py-2 px-4 rounded-md drop">
-            <ul class="flex flex-col">
-                <li class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left">
-                    Option one
-                </li>
-                <li class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left">
-                    Option two
-                </li>
-                <li class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left">
-                    Option three
-                </li>
-                <li class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left">
-                    Option four
-                </li>
-            </ul>
-        </div>
-    {/if}
+	<p class="text-neutral-300 text-sm font-semibold">Select</p>
+	<img src="/chevron.svg" alt="Down Icon" class={dropDownOpen ? "h-4 rotate-180": "h-4"} />
+	{#if dropDownOpen}
+		<div class="absolute top-full -left-4 bg-neutral-700 w-full py-2 px-2 rounded-tr-none rounded-tl-none rounded-br-md rounded-bl-md drop-shadow-sm">
+			<ul class="flex flex-col">
+				<li
+					class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left flex items-center space-x-2"
+				>
+					<img src="/graph-up.svg" alt="Beaker Icon" class="h-3 mt-[1px]" />
+					<p class="text-white text-sm font-semibold">Demo</p>
+				</li>
+				<li
+					class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left flex items-center space-x-2"
+				>
+					<img src="/coins.svg" alt="Beaker Icon" class="h-3 mt-[1px]" />
+					<p class="text-white text-sm font-semibold">Day Trading</p>
+				</li>
+				<li
+					class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left flex items-center space-x-2"
+				>
+					<img src="/piggy-bank.svg" alt="Beaker Icon" class="h-3 mt-[1px]" />
+					<p class="text-white text-sm font-semibold">Long Term</p>
+				</li>
+				<li
+					class="py-2 px-4 my-1 hover:bg-neutral-600 rounded-md w-full text-left flex items-center space-x-2"
+				>
+					<img src="/beaker.svg" alt="Beaker Icon" class="h-3 mt-[1px]" />
+					<p class="text-white text-sm font-semibold">Research</p>
+				</li>
+			</ul>
+		</div>
+	{/if}
 </button>
